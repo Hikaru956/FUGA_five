a3ed5f363a3a8edf9807c23352b2d15132e4105b
Hikaru956 <hikaru@956.jp>
2022-09-30 16:26:57 +0900

    database setting update.

--- db/schema.rb
+++ db/schema.rb
@@ -134,16 +134,6 @@ ActiveRecord::Schema.define(version: 2022_09_07_022542) do
     t.index ["shop_id"], name: "index_customers_on_shop_id"
   end
 
-  create_table "inquiries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "shop_id"
-    t.string "email"
-    t.text "name"
-    t.text "body"
-    t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "layout_schemes", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name"
     t.text "description"
