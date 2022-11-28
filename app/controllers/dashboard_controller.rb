27e471e1027c47c6ee2c32b8d0821144c4fc9d06
956 <rep@956.jp>
2022-11-28 15:47:33 +0900

    UI FIX

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,7 +9,8 @@
 #
 # require 'file_column'
 class DashboardController < ApplicationController
-  before_action :authenticate_user!, :peek_params
+  before_action :authenticate_user!, except: :index
+  before_action :peek_params
   #hikaru
   #before_action :check_super_privilege
   #before_action :check_super_privilege #hikaru :login_required,
