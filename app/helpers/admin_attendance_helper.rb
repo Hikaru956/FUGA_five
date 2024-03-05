c86632b2f11a7ce36185788735b397b8cf510926
956 <rep@956.jp>
2024-03-05 15:10:29 +0900

    UI FIX

--- app/helpers/admin_attendance_helper.rb
+++ app/helpers/admin_attendance_helper.rb
@@ -6,20 +6,18 @@ module AdminAttendanceHelper
     def calendar_mark_type(mark_type, show_none=false)
         case mark_type
         when CalendarMark::TYPE_NONE
-            return (show_none)? '(非表示)': "<span class='text-secondary'></span>".html_safe
+            return (show_none)? '(非表示)': ''
         when CalendarMark::TYPE_CIRCLE
-            return "<span class='text-primary'>◎</span>".html_safe
+            return "<span style='color: #00F;'>◎</span>".html_safe
         when CalendarMark::TYPE_TRIANGLE
-            return "<span class='text-warning'>△</span>".html_safe
+            return "<span style='color: #F00;'>△</span>".html_safe
         when CalendarMark::TYPE_BATSU
-            return "<span class='text-secondary'>✕</span>".html_safe
+            return "<span style='color: #AAA;'>✕</span>".html_safe
         end
         return ''
     end
 
     def calendar_mark_on(target_date, staff)
-        #marks = [CalendarMark::TYPE_CIRCLE, CalendarMark::TYPE_TRIANGLE, CalendarMark::TYPE_BATSU]
-        #calendar_mark_type(marks[rand(0..2)])
         cm = staff.calendar_marks.find_by(target_date: target_date)
         (cm.blank? || cm.mark_type==CalendarMark::TYPE_NONE)? nil: calendar_mark_type(cm.mark_type)
     end
