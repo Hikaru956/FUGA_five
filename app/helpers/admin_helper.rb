cfebfd4d907213a371d86860c655ac0b5a6a04e9
Hikaru956 <hikaru@956.jp>
2024-03-11 09:40:50 +0900

    add icon.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,5 @@
 module AdminHelper
+    def editor_six_icon(title='リッチテキストエディター',icon_size='');    sprintf("<i class='fa-regular fa-newspaper %s' title=%s></i>", icon_size, title).html_safe; end
+    def attendance_six_icon(title='勤怠管理'); sprintf('<i class="fa-solid fa-calendar-days" title=%s></i>', title).html_safe; end
+    def reservation_six_icon(title='予約'); sprintf('<i class="fa-solid fa-calendar-check" title=%s></i>', title).html_safe; end
 end
