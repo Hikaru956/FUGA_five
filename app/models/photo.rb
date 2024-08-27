a6ef448a127b5b1fb8bfb709542976986e0aefde
Hikaru956 <hikaru@956.jp>
2024-08-27 09:32:10 +0900

    add link.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -24,6 +24,15 @@ class Photo < ApplicationRecord
                     path: "#{Rails.root}/public/photo/image/clip/:id/:style/:filename"
   validates_attachment_content_type :clip, content_type: /\Aimage\/.*\z/
 
+  #has_attached_file :clip,
+  #                  styles: { large: "4096x4096>", thumb: "320x180>", panel: "640x480" },
+  #                  url: "/photo/image/clip/:id/:style/:filename",
+  #                  path: "#{Rails.root}/public/photo/image/clip/:id/:style/:filename"
+  #validates_attachment_content_type :clip, content_type: ["image/jpeg", "image/gif", "image/png", "image/webp"]
+
+
+
+
   #hikaru
   #file_column :image, :magick => { # :size => "800>",
   #  :versions => {
